6d7790285b3ceb09f9f004fa157ab0c4e44e562d
Lukas Rasocha <rasocha.lukas@gmail.com>
2023-10-25 00:31:05 +0200

    refactor(utils.py): rename load_raw_data() to load_data() and add support for loading processed data docs(utils.py): add docstring to load_data() explaining its arguments and purpose

--- src/utils.py
+++ src/utils.py
@@ -2,8 +2,16 @@ import pandas as pd
 from langdetect import detect
 
 
-def load_raw_data():
-  df = pd.read_csv('data/raw/jobs.csv', sep=';')
+def load_data(kind="processed"):
+  """
+  Load the data from the data folder.
+  args:
+    kind: "raw" or "processed"
+  """
+  if kind == "raw":
+    df = pd.read_csv('data/raw/jobs.csv', sep=';')
+  elif kind == "processed":
+    df = pd.read_csv('data/processed/cleaned_jobs.csv', sep=';')
   return df
 
 
