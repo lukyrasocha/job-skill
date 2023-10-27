9394495e32f4c1b12836b055f3c57bbcbb8ecede
Lukas Rasocha <rasocha.lukas@gmail.com>
2023-10-27 18:38:42 +0200

    feat(preprocessing): finalize the preprocessing, split the combined words, remove punctuation, add logging

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -4,13 +4,16 @@
 # nltk.download('stopwords')
 # nltk.download('wordnet')
 
-
 import pandas as pd
+import re
+
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+from tqdm import tqdm
 
 from src.utils import load_data, is_english
+from src.logger import working_on
 
 
 def convert_date_posted(date_str, date_scraped):
@@ -22,10 +25,34 @@ def convert_date_posted(date_str, date_scraped):
     return date_scraped  # If the format is not "x days ago", use the scraped date
 
 
+def split_combined_words(text):
+  """
+  Since during the scraping, some words are combined, e.g. "requirementsYou're" or "offerings.If" we need to split them
+  Splits words at:
+  1. Punctuation marks followed by capital letters.
+  2. Lowercase letters followed by uppercase letters.
+  """
+  # 1. split
+  text = re.sub(r'([!?,.;:])([A-Z])', r'\1 \2', text)
+
+  # 2. split
+  text = re.sub(r'([a-z])([A-Z])', r'\1 \2', text)
+
+  return text
+
+
 def text_preprocessing(text):
   """
-  Preprocesses text by tokenizing, removing stopwords and lemmatizing.
+  Preprocesses text by:
+    - Splitting combined words
+    - Tokenizing
+    - Removing stopwords
+    - Remove punctuation
+    - Lemmatizing
   """
+
+  text = split_combined_words(text)
+
   tokens = word_tokenize(text)
 
   stop_words = set(stopwords.words('english'))
@@ -33,6 +60,10 @@ def text_preprocessing(text):
 
   lemmatizer = WordNetLemmatizer()
   tokens = [lemmatizer.lemmatize(w) for w in tokens]
+
+  punctuation = ['!', ',', '.', ';', ':', '?', '(', ')', '[', ']']
+
+  tokens = [w.lower() for w in tokens if w not in punctuation]
 
   return tokens
 
@@ -43,35 +74,40 @@ def main():
   Loads the raw data and does the following:
   - Checks for english language
   - Removes rows with missing descriptions
+  - Inferes the date posted
+  - Preprocesses the description
+  - Saves the preprocessed data to data/processed/cleaned_jobs.csv
   """
 
+  working_on("Loading data")
   df = load_data(kind="raw")
 
   # Filter out jobs with missing descriptions
   df = df[df['description'].notna()]
 
-  # Filter out non-english descriptions
+  working_on("Filtering out non-english descriptions ...")
   for index, row in df.iterrows():
     if not is_english(row['description'][:100]):
       df.drop(index, inplace=True)
 
-  # Convert date_posted to a uniform format
-
+  working_on("Infering dates ...")
   df['date_posted'] = df.apply(lambda x: convert_date_posted(
       x['date_posted'], x['date_scraped']), axis=1)
 
   # Lower case all text
-  df['description'] = df['description'].str.lower()
   df['title'] = df['title'].str.lower()
   df['function'] = df['function'].str.lower()
   df['industries'] = df['industries'].str.lower()
 
-  df['description'] = df['description'].apply(text_preprocessing)
+  tqdm.pandas(desc="🐼 Preprocessing description", ascii=True, colour="#0077B5")
 
+  df['description'] = df['description'].progress_apply(text_preprocessing)
+
+  working_on("Saving preprocessed data ...")
   df.to_csv('data/processed/cleaned_jobs.csv', index=False, sep=';')
 
 
 if __name__ == "__main__":
-  # main()
+  #main()
   df = load_data(kind="processed")
-  print(df.iloc[500]["description"])
+  print(df.iloc[100])
