6fb9e0eb538d1fb90822f6ce52c8001b16f79183
Lukas Rasocha <rasocha.lukas@gmail.com>
2023-11-21 22:12:17 +0100

    feat(main.py): add argparse support for scraping data feat(main.py): add loading data functionality feat(main.py): add TFIDF clustering functionality feat(main.py): add visualization of TFIDF clusters

--- src/main.py
+++ src/main.py
@@ -2,18 +2,45 @@
 MAIN ENTRYPOINT OF THE ENTIRE PROJECT
 """
 
+import argparse
+import ast
+
+from utils import load_data, visualize_cluster
+from tfidf_cluster import TFIDF_cluster
+from logger import working_on, success, info
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--scrape", help="Scrape data",
+                    action="store_true", default=False)
+args = parser.parse_args()
+
 
 def main():
   """SCRAPING"""
-  # IF SCRAPE == TRUE: By default it will be false
+  # x = input("Do you wish to scrape new data? (y/n)")
+  if args.scrape:
+    # Scrape data
+    # Save data
+    print("true")
+    pass
 
   """PREPROCESSING"""
 
   """LOAD CLEAN DATA"""
+  working_on("Loading data")
+  data = load_data(kind="processed")
+  data["description"] = data["description"].apply(ast.literal_eval)
+  success("Data loaded")
 
   """TF IDF CLUSTERING"""
-  # Cluster
-  # Save clusters
+  working_on("TFIDF Clustering")
+  tfidf_clusters, tfidf_matrix = TFIDF_cluster(data, save_clusters=False)
+  visualize_cluster(tfidf_matrix,
+                    tfidf_clusters["cluster"].to_numpy(),
+                    savefig=True,
+                    filename="tfidf_clusters.png",
+                    name="TFIDF Clustering")
+  tfidf_matrix = 0
 
   """WORD2VEC CLUSTERING"""
   # Cluster
