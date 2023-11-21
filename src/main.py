ad23f92b432af4542e88ee2d2e59e914071b8c94
Lukas Rasocha <rasocha.lukas@gmail.com>
2023-11-21 19:02:44 +0100

    feat(MAIN): add main entrypoint of our project, to be filled with the individual sections, so you can just run main.py and rerun the entirity of the project with nice logging and stuff

---
+++ src/main.py
@@ -0,0 +1,45 @@
+"""
+MAIN ENTRYPOINT OF THE ENTIRE PROJECT
+"""
+
+
+def main():
+  """SCRAPING"""
+  # IF SCRAPE == TRUE: By default it will be false
+
+  """PREPROCESSING"""
+
+  """TF IDF CLUSTERING"""
+  # Load data
+  # Cluster
+  # Save clusters
+
+  """DOC2VEC CLUSTERING"""
+  # Load data
+  # Cluster
+  # Save clusters
+
+  """SIMILARITY CLUSTERING"""
+  # Load data
+  # Cluster
+  # Save clusters
+
+  """EVALUATION"""
+  # Load clusters
+  # Load ground truth
+  # Evaluate
+  # Select best clustering
+  # Calculate DBINDEX
+  # Visualize clusters
+
+  """GROUND TRUTH INFERENCE"""
+  # SET ENV VARIABLE OPEN_AI_KEY TO YOUR OPEN AI KEY
+  # SAVE GROUND TRUTH
+
+  """SKILL EXTRACTION"""
+  # SET ENV VARIABLE OPEN_AI_KEY TO YOUR OPEN AI KEY
+  # SAVE SKILLS FOR EACH CLUSTERK
+
+
+if __name__ == "__main__":
+  main()
