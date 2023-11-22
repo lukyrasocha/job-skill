17a8f62d92d416c6189ba54df4916305075eb320
truszkowski-tomasz <79571804+truszkowski-tomasz@users.noreply.github.com>
2023-11-22 18:04:05 +0100

    tfidf for functions and industries only

--- src/tfidf_cluster.py
+++ src/tfidf_cluster.py
@@ -30,10 +30,33 @@ def TFIDF_cluster(data, save_clusters=True):
 
   return data[["id", "cluster"]], tfidf_matrix.toarray()
 
+def TFIDF_industries_and_functions_cluster(data, save_clusters=False):
+  """
+  data: pandas dataframe (cleaned jobs)
+  save_clusters: Boolean, if True, save the clusters to a csv file in a format "id, cluster"
+  """
+
+  data["industries"] = data['function'] + ', ' + data['industries']
+  data['industries'] = data['industries'].str.replace(',,', ',', regex=False)
+
+  tfidf_matrix = apply_tftidf(data["industries"])
+
+  data["cluster"] = apply_kmeans(tfidf_matrix, k=20)
+
+  if save_clusters:
+    data[["id", "cluster"]].to_csv("clusters/tfidf_industries_and_functions_clusters.csv", index=False)
+
+  dbs = round(davies_bouldin_score(tfidf_matrix.toarray(), data["cluster"]), 3)
+
+  success("David Bouldin score: " + str(dbs))
+
+  return data[["id", "cluster"]], tfidf_matrix.toarray()
+
 
 if __name__ == "__main__":
   data = load_data(kind="processed")
   data["description"] = data["description"].apply(ast.literal_eval)
-  clusters = TFIDF_cluster(data, save=False)
+  clusters_text = TFIDF_cluster(data, save=False)
+  clusters_industries = TFIDF_industries_and_functions_cluster(data, save_clusters=False)
 
-  print(clusters)
+  print(clusters_text, clusters_industries)
