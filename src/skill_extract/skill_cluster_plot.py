a3e925fa82cfb4fb91496290d09430c76768a79c
tinghui8576 <tinghui1738cheng@gmail.com>
2023-11-27 00:32:07 +0100

    Update skills comparison with matching the most similarity value

--- src/skill_extract/skill_cluster_plot.py
+++ src/skill_extract/skill_cluster_plot.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 from src.helper.logger import working_on
 from src.helper.utils import load_data
 from nltk.stem import WordNetLemmatizer
-
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 def count_words(words):
 
@@ -45,7 +46,7 @@ def plot_top(words, key, ax, top_n):
 
   ax.bar(words, frequencies, color='dodgerblue')
   ax.set_title(f'Top {top_n} Word Frequency')
-  ax.set_title(f'Cluster{key}')
+  ax.set_title(key)
   return ax
 
 
@@ -54,7 +55,7 @@ def plot_wordcloud(words, key, ax):
                         background_color='white').generate_from_frequencies(words)
   ax.imshow(wordcloud, interpolation='bilinear')
   ax.axis('off')
-  ax.set_title(f' Cluster {key}')
+  ax.set_title(key)
   return ax
 
 
@@ -73,7 +74,7 @@ def plot_frequency_histogram(skills, filename, top_n=5):
 
   # Plot each dictionary on a subplot
   for i, (key, word_frequency) in enumerate(skills.items()):
-    axs[i] = plot_top(word_frequency, key, axs[i], top_n)
+    axs[i] = plot_top(word_frequency, 'Cluster {}'.format(key), axs[i], top_n)
 
   # Adjust layout
   plt.tight_layout(rect=[0.04, 0, 1, 0.96])
@@ -90,7 +91,7 @@ def plot_frequency_histogram(skills, filename, top_n=5):
   axs = axs.flatten()
   # Generate word clouds for each inner dictionary and plot on a subplot
   for i, (key, word_frequency) in enumerate(skills.items()):
-    axs[i] = plot_wordcloud(word_frequency, key, axs[i])
+    axs[i] = plot_wordcloud(word_frequency,  'Cluster {}'.format(key), axs[i])
 
   # Remove empty subplots if needed
   for j in range(i + 1, num_rows * num_cols):
@@ -103,61 +104,42 @@ def plot_frequency_histogram(skills, filename, top_n=5):
   fig.savefig('figures/'+filename+'_wordcloud.jpg')
 
 
-def plot_compared(gt, compare, top_n=5):
-  N = len(gt)
+def plot_compared(gt, compare, file, top_n=5):
   num_cols = 2  # Number of columns in the subplot grid
-  num_rows = N  # Ceiling division to determine the number of rows
+  num_rows = 1  # Ceiling division to determine the number of rows
 
   # Create subplots
-  fig, axs = plt.subplots(nrows=num_rows, ncols=num_cols, figsize=(24, 5*N))
-  fig.suptitle('Word Frequency in each cluster', fontsize=25)
+  fig, axs = plt.subplots(nrows=num_rows, ncols=num_cols, figsize=(20, 6))
+  fig.suptitle('Word Frequency in {}'.format(file), fontsize=20)
 
   # Flatten the axs array for easier indexing
   axs = axs.flatten()
   # Plot each dictionary on a subplot
-  i = 0
-  for key, word_frequency in gt.items():
-    axs[i] = plot_top(word_frequency, key, axs[i], top_n)
-    axs[i+1] = plot_top(compare[key], key, axs[i+1], top_n)
-    i += 2
+  axs[0] = plot_top(gt, 'Ground Truth', axs[0], top_n)
+  axs[1] = plot_top(compare, 'The Comparing Cluster', axs[1], top_n)
 
   # Adjust layout
-  plt.figtext(0.3, 0.97, "Ground truth", ha="center",
-              va="top", fontsize=20, color="black")
-  plt.figtext(0.8, 0.97, "The comparing result", ha="center",
-              va="top", fontsize=20, color="black")
-  plt.figtext(0.025, 0.5, 'frequency', ha="center",
-              va="top", fontsize=20, rotation='vertical')
+  plt.figtext(0.025, 0.6, 'frequency', ha="center",
+              va="top", fontsize=15, rotation='vertical')
   plt.tight_layout(rect=[0.04, 0, 1, 0.96])
 
   # Show the plot
-  fig.savefig('figures/compared_histogram.jpg')
+  fig.savefig('figures/compared_histogram_'+ file+ '.jpg')
 
   # Create subplots
-  fig, axs = plt.subplots(nrows=num_rows, ncols=num_cols, figsize=(24, 5*N))
-  fig.suptitle('Word Cloud Subplots', fontsize=25)
+  fig, axs = plt.subplots(nrows=num_rows, ncols=num_cols, figsize=(20, 6))
+  fig.suptitle('Word Cloud Subplots in {}'.format(file), fontsize=20)
 
   # Flatten the axs array for easier indexing
   axs = axs.flatten()
   # Generate word clouds for each inner dictionary and plot on a subplot
-  i = 0
-  for key, word_frequency in gt.items():
-    axs[i] = plot_wordcloud(word_frequency, key, axs[i])
-    axs[i+1] = plot_wordcloud(compare[key], key, axs[i+1])
-    i += 2
-
-  # Remove empty subplots if needed
-  for j in range(i + 1, num_rows * num_cols):
-    fig.delaxes(axs[j])
+  axs[0] = plot_wordcloud(gt,'Ground Truth', axs[0])
+  axs[1] = plot_wordcloud(compare, 'The Comparing Cluster', axs[1])
 
   # Adjust layout
-  plt.figtext(0.3, 0.97, "Ground truth", ha="center",
-              va="top", fontsize=20, color="black")
-  plt.figtext(0.8, 0.97, "The comparing result", ha="center",
-              va="top", fontsize=20, color="black")
-  plt.tight_layout(rect=[0.04, 0, 1, 0.96])
+  plt.tight_layout(rect=[0.01, 0, 1, 0.96])
   # Show the plot
-  fig.savefig('figures/compared_wordcloud.jpg')
+  fig.savefig('figures/compared_wordcloud_'+ file+ '.jpg')
 
 
 def skill_analysis(compare, gt=pd.DataFrame()):
@@ -175,10 +157,33 @@ def skill_analysis(compare, gt=pd.DataFrame()):
     plot_frequency_histogram(cluster_skill, 'cluster', 5)
 
   if not gt.empty and not compare.empty:
-    plot_compared(gt_skill, cluster_skill)
+    match_id = {}
+    for i in set(gt['cluster']):
+        gt_id = gt.loc[gt['cluster']==i]['id']
+        
+        X = skills[skills['id'].isin(gt_id)]['skills'].values[0][1:-1]
+        
+
+        similarity = np.empty(20)
+        for j in set(compare['cluster']):
+            compare_id = compare.loc[compare['cluster']==j]['id']
+            Y= skills[skills['id'].isin(compare_id)]['skills'].values[0][1:-1]
+            
+        
+            # Consine similarity 
+            vectorizer = CountVectorizer()
+            vec = vectorizer.fit_transform([X,Y])
+            similarity[j] = cosine_similarity(vec[0], vec[1])[0][0]
+        
+        category = gt.loc[gt['cluster']==i]['category'].unique()
+        match_id[i] = similarity.argmax()
+
+        if similarity[match_id[i]] > 0.8:
+            plot_compared(gt_skill[i], cluster_skill[match_id[i]], category[0])
+
 
 
 if __name__ == "__main__":
-  gt = load_data(kind="ground_truth")
+  gt = load_data(kind="ground_truth_gpt")
   compare = pd.read_csv('clusters/tfidf_noun_clusters.csv')
   skill_analysis(gt, compare)
